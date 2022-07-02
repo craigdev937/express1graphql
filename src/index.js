d3c4970d6cc0f5f86467aaaadb43cb9878b98d3d
Craig <indigomx9@gmail.com>
2022-07-02 16:34:11 -0400

    Added Express GraphQL and sample data.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { schema } from "./graphql/schema.js";
 
 (async () => {
     const app = express();
 
-    app.get("/", (req, res) => res.json({
-        hello: "world!"
+    app.use("/graphql", graphqlHTTP({
+        schema, graphiql: true
     }));
 
     const port = process.env.PORT;
